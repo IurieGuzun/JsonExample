4cd67577e58b1ddb369211c58d947de412dbf49d
Iurie Guzun <iurie_guzun@yahoo.com>
2021-04-22 12:17:07 -0400

    Move private functions !

---
+++ JsonExample/DemoData.swift
@@ -0,0 +1,14 @@
+//
+//  DemoData.swift
+//  JsonExample
+//
+//  Created by Iurie Guzun on 2021-04-22.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import Foundation
+struct DemoData: Codable {
+    let title: String
+    let description: String
+}
+
--- JsonExample/ViewController.swift
+++ JsonExample/ViewController.swift
@@ -13,8 +13,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Hi")
+        if let localData = self.readLocalFile(forName: "data") {
+            self.parse(jsonData: localData)
+        }
     }
 
+    private func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+    private func parse(jsonData: Data) {
+        do {
+            let decodedData = try JSONDecoder().decode(DemoData.self,
+                                                       from: jsonData)
+            
+            print("Title: ", decodedData.title)
+            print("Description: ", decodedData.description)
+            print("===================================")
+        } catch {
+            print("decode error")
+        }
+    }
+    private func loadJson(fromURLString urlString: String,
+                          completion: @escaping (Result<Data, Error>) -> Void) {
+        if let url = URL(string: urlString) {
+            let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                
+                if let data = data {
+                    completion(.success(data))
+                }
+            }
+            
+            urlSession.resume()
+        }
+    }
 
 }
 
